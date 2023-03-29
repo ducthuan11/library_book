7fa9fa9527860d6a7d63592aceda53d37d7c292a
ducthuan11 <ducthuan110199@gmail.com>
2023-03-29 15:00:52 +0700

    feat: API update delete for publisher

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -3,19 +3,63 @@
 namespace App\Http\Controllers;
 
 use App\Models\Publisher;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class PublisherController extends Controller
 {
-    public function show(){
+    public function show()
+    {
         $publishers = Publisher::all();
         return $publishers;
     }
-    public function create(Request $request){
-        Publisher::create(
-            [
-                'name' => $request->get('name'),
-            ]
+    public function create(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        try {
+            Publisher::create(
+                [
+                    'name' => $request->get('name'),
+                ]
             );
+        } catch (Exception $e) {
+            return Response::json([
+                'message' => $e->getMessage(),
+            ]);
+        };
+
+        return Response::json([
+            'message' => 'Successfully created'
+        ]);
     }
-}      
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+        ]);
+        try {
+            $publisher = Publisher::find($request->route('id'));
+            $publisher->name = $request->get('name');
+            $publisher->save();
+        } catch (Exception $e) {
+            return Response::json([
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+    public function detail(Request $request)
+    {
+        $publisher = Publisher::find($request->route('id'));
+        return $publisher;
+    }
+    public function delete(Request $request)
+    {
+        
+            $publisher = Publisher::find($request->route('id'));
+            $publisher->delete();
+    }
+}
