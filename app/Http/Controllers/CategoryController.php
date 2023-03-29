4d6eb384560dfd05eb769f0534aa5d642eea1512
ducthuan11 <ducthuan110199@gmail.com>
2023-03-29 15:00:17 +0700

    feat: API update delete for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,19 +3,63 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class CategoryController extends Controller
 {
-    public function show(){
+    public function show()
+    {
         $categories = Category::all();
         return $categories;
     }
-    public function  create(Request $request){
-        Category::create(
-            [
-                'name' => $request->get('name'),
-            ]
-        );
+    public function  create(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        try {
+            Category::create(
+                [
+                    'name' => $request->get('name'),
+                ]
+            );
+        } catch (Exception $e) {
+            return Response::json([
+                'message' => $e->getMessage(),
+            ]);
+        };
+        return Response::json([
+            'message' => 'Successfully created'
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' =>   'required|string',
+        ]);
+        try {
+            $category = Category::find($request->route('id'));
+
+            $category->name = $request->get('name');
+            $category->save();
+        } catch (Exception $e) {
+            return Response::json([
+                'message' => $e->getMessage(),
+            ]);
+        };
+    }
+    public function detail(Request $request)
+    {
+        $category = Category::find($request->route('id'));
+        return $category;
+    }
+    public function delete(Request $request)
+    {
+        $category = Category::find($request->route('id'));
+        $category->delete();
     }
 }
