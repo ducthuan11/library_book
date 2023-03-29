3600116246fffc8d16b06fd23a5797e866022b82
ducthuan11 <ducthuan110199@gmail.com>
2023-03-29 15:03:35 +0700

    chore: clean code

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PublisherController;
-use App\Http\Controllers\Book_Category_GroupController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,15 +20,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/publishers', [PublisherController::class, 'show']);
-Route::get('/publishers', [PublisherController::class, 'create']);
-
-Route::get('/books', [BookController::class, 'show']);
-Route::post('/books', [BookController::class, 'create']);
-
-Route::get('/categories', [CategoryController::class, 'show']);
-Route::post('/categories', [CategoryController::class, 'create']);
-
-Route::get('/book_category_groups', [Book_Category_GroupController::class, 'show']);
-Route::get('/book_category_groups', [Book_Category_GroupController::class, 'create']);
+ 
 
