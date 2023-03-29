55abca4942147aea299a35099da98a3f4833815a
ducthuan11 <ducthuan110199@gmail.com>
2023-03-29 15:06:01 +0700

    feat: CRUD book category publisher

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PublisherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +20,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/publisher', [PublisherController::class, 'show']);
+Route::get('/publisher/{id}', [PublisherController::class,'detail']);
+Route::post('/publisher', [PublisherController::class, 'create']);
+Route::put('/publisher/{id}', [PublisherController::class,'update']);
+Route::delete('/publisher/{id}', [PublisherController::class,'delete']);
+
+
+Route::get('/book', [BookController::class, 'show']);
+Route::get('/book/{id}', [BookController::class, 'detail']);
+Route::post('/book', [BookController::class, 'create']);
+Route::put('/book/{id}', [BookController::class, 'update']);
+Route::delete('/book/{id}',[BookController::class, 'delete']);
+
+Route::get('/category', [CategoryController::class, 'show']);
+Route::get('/category/{id}', [CategoryController::class, 'detail']);
+Route::post('/category', [CategoryController::class, 'create']);
+Route::put('/category/{id}', [CategoryController::class, 'update']);
+Route::delete('/category/{id}', [CategoryController::class, 'delete']);
+
+
